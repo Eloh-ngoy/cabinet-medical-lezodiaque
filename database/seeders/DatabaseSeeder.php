eb43ab3631b7a934368735bf77cf3cef41ada99e
Eloh-Ngoy <elohimngoy03@email.com>
2026-07-14 11:31:00 +0200

    Fix DatabaseSeeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,24 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
 use App\Models\User;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
-public function run(): void
+class DatabaseSeeder extends Seeder
 {
-$role = Role::firstOrCreate([
-'name' => 'admin',
-'guard_name' => 'web'
-]);
+    public function run(): void
+    {
+        $role = Role::firstOrCreate([
+            'name' => 'admin',
+            'guard_name' => 'web'
+        ]);
 
-$user = User::firstOrCreate(
-['username' => 'admin'],
-[
-'email' => 'admin@lezodiaque.com',
-'full_name' => 'Dr. Directeur Général LEZODIAQUE',
-'matricule' => 'MED-001',
-'password' => Hash::make('admin123'),
-'must_change_password' => false,
-]
-);
+        $user = User::firstOrCreate(
+            [
+                'username' => 'admin'
+            ],
+            [
+                'email' => 'admin@lezodiaque.com',
+                'full_name' => 'Dr. Directeur Général LEZODIAQUE',
+                'matricule' => 'MED-001',
+                'password' => Hash::make('admin123'),
+                'must_change_password' => false,
+            ]
+        );
 
-$user->assignRole($role);
+        $user->assignRole($role);
+    }
 }
